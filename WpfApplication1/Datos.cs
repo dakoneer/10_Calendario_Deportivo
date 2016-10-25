155ab16fba7a157865580e53187af14b17fc0b21
David <David@DESKTOP-VGI3RVU>
2016-10-25 22:11:12 +0200

    Agregar archivos de proyecto.

---
+++ WpfApplication1/Datos.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    public class Datos
+    {
+        static List<string> equiposOriginal = new List<string>();
+
+        static List<int> listaLocales = new List<int>();
+        static List<int> listaVisitantes = new List<int>();
+
+        static List<Partido> jornada = new List<Partido>();
+
+        public static List<string> EquiposOriginal
+        {
+            get
+            {
+                return equiposOriginal;
+            }
+
+            set
+            {
+                equiposOriginal = value;
+            }
+        }
+
+        public static List<int> ListaLocales
+        {
+            get
+            {
+                return listaLocales;
+            }
+
+            set
+            {
+                listaLocales = value;
+            }
+        }
+
+        public static List<int> ListaVisitantes
+        {
+            get
+            {
+                return listaVisitantes;
+            }
+
+            set
+            {
+                listaVisitantes = value;
+            }
+        }
+
+        public static List<Partido> Jornada
+        {
+            get
+            {
+                return jornada;
+            }
+
+            set
+            {
+                jornada = value;
+            }
+        }
+        //Cargar equipos en lista
+        public static void cargarEquipos(string path)
+        {
+            EquiposOriginal.Clear();
+            StreamReader reader = new StreamReader(path, Encoding.Default);
+            while (reader.Peek() > -1)
+            {
+                EquiposOriginal.Add(reader.ReadLine());
+            }
+            reader.Close();
+        }
+        //Generacion de listas auxiliares (local y visitante)
+        public static void generarListasAuxiliares()
+        {
+            for (int i = 1; i <= EquiposOriginal.Count - 2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ListaVisitantes.Add(i);
+                }
+                else
+                {
+                    ListaLocales.Add(i);
+                }
+            }
+            ListaLocales.Add(EquiposOriginal.Count);
+            ListaVisitantes.Add(EquiposOriginal.Count - 1);
+        }
+        //mover las listas de la primera jornada
+        public static void moverListasPrimeraJornada()
+        {
+            for (int i = 1; i <= Datos.EquiposOriginal.Count - 2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Datos.Jornada.Last().EquipoVisitante = Datos.EquiposOriginal.ElementAt(i - 1);
+                }
+                else
+                {
+                    Datos.Jornada.Add(new Partido(Datos.EquiposOriginal.ElementAt(i - 1), ""));
+                }
+            }
+
+            Datos.Jornada.Add(new Partido(Datos.EquiposOriginal.Last(), Datos.EquiposOriginal.ElementAt(Datos.EquiposOriginal.LastIndexOf(Datos.EquiposOriginal.Last()) - 1)));
+        }
+        //mover las listas para las jornadas pares
+        public static void moverListasPares()
+        {
+            ListaLocales.Insert(0, listaLocales.Last());
+            ListaLocales.RemoveAt(listaLocales.Count - 1);
+            getListas();
+        }
+        //mover las listas para las jornadas impares
+        public static void moverListasImpares()
+        {
+            ListaVisitantes.Insert(ListaVisitantes.Count, ListaLocales.Last());
+            ListaLocales.RemoveAt(listaLocales.Count - 1);
+            listaLocales.Add(listaLocales.FirstOrDefault());
+            listaLocales.RemoveAt(0);
+            listaLocales.Insert(0,ListaVisitantes.FirstOrDefault());
+            ListaVisitantes.RemoveAt(0);
+            getListas();
+        }
+        //equiparacion de listas auxiliares con la lista original
+        public static void getListas()
+        {
+            Datos.Jornada.Clear();
+
+            int local;
+            int visitante;
+            string uno = "";
+            string dos = "";
+
+            for (int i = 0; i < EquiposOriginal.Count / 2; i++)
+            {
+                local = ListaLocales.ElementAt(i);
+                visitante = ListaVisitantes.ElementAt(i);
+
+                foreach (string s in EquiposOriginal)
+                {
+                    string[] indices = s.Split('-');
+
+                    if (local.ToString() == indices[0])
+                    {
+                        uno = s;
+                    }
+                    else if (visitante.ToString() == indices[0])
+                    {
+                        dos = s;
+                    }
+                }
+                Datos.Jornada.Add(new Partido(uno, dos));
+            }
+
+        }
+    }
+
+    public class Partido
+    {
+        string equipoLocal;
+        string equipoVisitante;
+
+        public Partido(string equipoLocal, string equipoVisitante)
+        {
+            this.equipoLocal = equipoLocal;
+            this.equipoVisitante = equipoVisitante;
+        }
+        public Partido()
+        {
+
+        }
+        public string EquipoLocal
+        {
+            get
+            {
+                return equipoLocal;
+            }
+
+            set
+            {
+                equipoLocal = value;
+            }
+        }
+
+        public string EquipoVisitante
+        {
+            get
+            {
+                return equipoVisitante;
+            }
+
+            set
+            {
+                equipoVisitante = value;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int contJornada = 0;
+
+        private void button_seleccionar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.ShowDialog();
+            textBox_seleccionar.Text = abrir.FileName;
+
+            //Añadir equipos a la lista
+            Datos.cargarEquipos(textBox_seleccionar.Text);
+
+            foreach (string c in Datos.EquiposOriginal)
+            {
+                this.listBox_equipos.Items.Add(c);
+            }
+
+            //Genero listas auxiliares
+            Datos.generarListasAuxiliares();
+        }
+
+        private void button_generarXML_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Partido>));
+            TextWriter textWriter = new StreamWriter(@"C:\Users\DAM220\Desktop\jornada" + contJornada + ".xml");
+            serializer.Serialize(textWriter, Datos.Jornada);
+            textWriter.Close();
+
+            //Pruebas en casa
+            //XmlSerializer serializer = new XmlSerializer(typeof(List<Partido>));
+            //TextWriter textWriter = new StreamWriter(@"C:\Users\David\Desktop\jornada" + contJornada + ".xml");
+            //serializer.Serialize(textWriter, Datos.Jornada);
+            //textWriter.Close();
+        }
+
+        private void button_calcular_Click(object sender, RoutedEventArgs e)
+        {
+            contJornada++;
+            //Comprobacion si ya se han generado todas las jornadas
+            if (contJornada > Datos.EquiposOriginal.Count / 2)
+            {
+                MessageBox.Show("Ya se han generado todas las jornadas.");
+            }
+            //Jornada 1
+            else if (contJornada == 1)
+            {
+                this.listBox_jornadas.Items.Clear();
+                this.listBox_jornadas.Items.Add("Jornada " + contJornada);
+                Datos.moverListasPrimeraJornada();
+
+                //Mostrar jornada en la lista
+                foreach (Partido p in Datos.Jornada)
+                {
+                    this.listBox_jornadas.Items.Add(p.EquipoLocal + "-" + p.EquipoVisitante);
+                }
+            }
+            else if (contJornada % 2 == 0)
+            {
+                this.listBox_jornadas.Items.Clear();
+
+                this.listBox_jornadas.Items.Add("Jornada " + contJornada);
+                Datos.moverListasPares();
+
+                //Mostrar jornada en la lista
+                foreach (Partido p in Datos.Jornada)
+                {
+                    this.listBox_jornadas.Items.Add(p.EquipoLocal + "-" + p.EquipoVisitante);
+                }
+            }
+            else if (contJornada %2 != 0)
+            {
+                this.listBox_jornadas.Items.Clear();
+
+                this.listBox_jornadas.Items.Add("Jornada " + contJornada);
+                Datos.moverListasImpares();
+
+                //Mostrar jornada en la lista
+                foreach (Partido p in Datos.Jornada)
+                {
+                    this.listBox_jornadas.Items.Add(p.EquipoLocal + "-" + p.EquipoVisitante);
+                }
+            }
+        }
+    }
+}
